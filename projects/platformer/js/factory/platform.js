2ab3513d315dd9691270a3085da57a6a1dde163b
Isela-Garza <isela.garza54@gmail.com>
2021-06-08 16:04:14 +0000

    add platforms and collectables to game

--- projects/platformer/js/factory/platform.js
+++ projects/platformer/js/factory/platform.js
@@ -17,3 +17,31 @@
         opspark.platform.create = create;
     };
 })(window);
+/*
+createPlatform(x, y, scaleX, scaleY);
+ 
+x: The x coordinate for the platform.
+y: The y coordinate for the platform.
+scaleX: OPTIONAL The scale factor on the x-axis, this value will stretch the platform in width.
+scaleY: OPTIONAL The scale factor on the y-axis, this value will stretch the platform in height.
+*/
+
+
+createPlatform(400, 460);           // normal platform
+createPlatform(300, 200, 0.3)       // small horizontal platform (30% the normal width)
+createPlatform(500, 500, 0.3, 10)   // tall vertical wall (30% the normal width and 10x the height)
+/*
+createCollectable(type, x, y, gravity, bounce);
+
+type: The type of the collectable. 
+x: The x coordineate for the collectable.
+y: The y coordineate for the collectable.
+gravity: OPTIONAL The gravitational pull on the collectable.
+bounce: OPTIONAL A factor effecting how much the collectable will bounce off platforms, etc.
+*/
+createCollectable(type.steve, 200, 170, 6, 0.7);
+type.db         // worth 10 points
+type.max        // worth 20 points
+type.steve      // worth 30 points
+type.grace      // worth 40 points
+type.kennedi    // worth 50 points
